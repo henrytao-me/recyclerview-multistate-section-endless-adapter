390652242caa78150afce32bfe44d44aabeff002
Henry Tao <hi@henrytao.me>
2015-10-08 15:26:45 +0700

    Bump to v1.3.0

---
+++ sample/src/main/java/me/henrytao/sample/adapter/HeaderFooterAdapter.java
@@ -0,0 +1,28 @@
+package me.henrytao.sample.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import me.henrytao.me.sample.R;
+import me.henrytao.recyclerview.SimpleRecyclerViewAdapter;
+
+/**
+ * Created by henrytao on 8/16/15.
+ */
+public class HeaderFooterAdapter extends SimpleRecyclerViewAdapter {
+
+  public HeaderFooterAdapter(RecyclerView.Adapter baseAdapter) {
+    super(baseAdapter);
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateFooterViewHolder(LayoutInflater inflater, ViewGroup parent) {
+    return new FooterHolder(inflater, parent, R.layout.holder_footer);
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateHeaderViewHolder(LayoutInflater inflater, ViewGroup parent) {
+    return new HeaderHolder(inflater, parent, R.layout.holder_header);
+  }
+}
---
+++ sample/src/main/java/me/henrytao/sample/adapter/MultipleHeaderAdapter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 "Henry Tao <hi@henrytao.me>"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.henrytao.sample.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import me.henrytao.me.sample.R;
+import me.henrytao.recyclerview.RecyclerViewAdapter;
+
+/**
+ * Created by henrytao on 8/16/15.
+ */
+public class MultipleHeaderAdapter extends RecyclerViewAdapter {
+
+  public MultipleHeaderAdapter(RecyclerView.Adapter baseAdapter) {
+    super(2, 0, baseAdapter);
+  }
+
+  @Override
+  public void onBindFooterViewHolder(RecyclerView.ViewHolder holder, int index) {
+
+  }
+
+  @Override
+  public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int index) {
+
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateFooterViewHolder(LayoutInflater inflater, ViewGroup parent, int index) {
+    return null;
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateHeaderViewHolder(LayoutInflater inflater, ViewGroup parent, int index) {
+    HeaderHolder holder = null;
+    switch (index) {
+      case 0:
+        holder = new HeaderHolder(inflater, parent, R.layout.holder_header);
+        break;
+      case 1:
+        holder = new HeaderHolder(inflater, parent, R.layout.holder_header);
+        ((TextView) holder.getItemView().findViewById(R.id.title)).setText("This is another header");
+        break;
+    }
+    return holder;
+  }
+}
